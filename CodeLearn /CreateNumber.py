6cc9b394a87d7cdf1fe3ebb5d928f74470b994d7
ThaiTOm <nguyenduythaise1@gmail.com>
2021-06-02 11:38:03 +0700

    main

---
+++ CodeLearn /CreateNumber.py
@@ -0,0 +1,16 @@
+def createNumber(n, k):
+    i = 1
+    ans = []
+    while i < 10:
+        print(i)
+        num =
+        if i + k <= 9:
+
+            return
+        if i - k >= 0:
+            return
+        i += 1
+    return
+
+
+createNumber(3, 7)
